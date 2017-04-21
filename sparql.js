605fd85d7342f8631e20a7537af4e8efc6d8847e
Taisuke Fukuno <fukuno@TPC0165.local>
2017-04-21 19:57:16 +0900

    add Kobe

--- sparql.js
+++ sparql.js
@@ -6,7 +6,8 @@ prefix ic: <http://imi.go.jp/ns/core/rdf#> // 2.3.2
 var SPARQL_ENDPOINT = [
 	{ name: "odp", baseurl: "https://sparql.odp.jig.jp/data/sparql", suffix: "&output=json", imi: "http://imi.ipa.go.jp/ns/core/rdf#" },
 	{ name: "Osaka", baseurl: "https://data.city.osaka.lg.jp/sparql", suffix: "&output=json", imi: "http://imi.ipa.go.jp/ns/core/rdf#" },
-	{ name: "Kyoto", baseurl: "https://sparql.city.kyoto.lg.jp/sparql/", suffix: "&should-sponge=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on", imi: "http://imi.go.jp/ns/core/rdf#" }
+	{ name: "Kyoto", baseurl: "https://sparql.city.kyoto.lg.jp/sparql/", suffix: "&should-sponge=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on", imi: "http://imi.go.jp/ns/core/rdf#" },
+	{ name: "Kobe", baseurl: "https://data.city.kobe.lg.jp/sparql", suffix: "&output=json", imi: "http://imi.go.jp/ns/core/rdf#" },
 ];
 
 var querySPARQL = function(endpoint, q, callback) {
